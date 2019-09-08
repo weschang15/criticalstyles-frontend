65e35912d4beb2f2791b39d7ec9f32751b3cef89
Wesley Chang <weschang15@gmail.com>
2019-09-07 23:10:38 -0700

    Added react-snap again

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
 import { ApolloProvider } from "@apollo/react-hooks";
 import "normalize.css";
 import React from "react";
-import { render } from "react-dom";
+import { hydrate, render } from "react-dom";
 import App from "./App";
 import { apolloClient } from "./config/apolloClient";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
-render(
-  <ApolloProvider client={apolloClient}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById("root")
-);
+const root = document.getElementById("root");
+if (root.hasChildNodes()) {
+  hydrate(
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>,
+    root
+  );
+} else {
+  render(
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>,
+    root
+  );
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
